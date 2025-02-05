c3ec13cfbddd6e77dcef32060b99755349c3159a
Гавриш Олександр <sashawwe21@gmail.com>
2025-02-05 16:53:24 +0200

    add

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  mongoose.connect(process.env.MONGODB_URI);
-  console.log('DB connecting');
-}
+  mongoose.connect(
+    process.env.MONGODB_URI ||
+      "mongodb+srv://Alex_Gavrish:IKc0xvjnoOshP9Vp@cluster0.sdvypro.mongodb.net/syner?retryWrites=true&w=majority&appName=Cluster0"
+  );
+  console.log("DB connecting");
+};
 
-module.exports = { connectDB }
+module.exports = { connectDB };
