3dbc34bd77576f72057fd3551c0a2f676cd7b00b
Гавриш Олександр <sashawwe21@gmail.com>
2025-02-06 15:56:46 +0200

    add directive param

--- src/models/UserForm.js
+++ src/models/UserForm.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: false,
     },
+    directive: {
+      type: String,
+      required: true
+    },
     done: {
       type: Boolean,
       default: false,
--- src/routes/userForm.js
+++ src/routes/userForm.js
@@ -6,37 +6,31 @@ const axios = require("axios");
 // Роут для створення нового користувача та відправки повідомлення в Telegram
 router.post("/register", async (req, res) => {
   try {
-    const { name, email, phone, date, project } = req.body;
+    const { name, email, phone, date, directive, project } = req.body;
 
     // Створення нового користувача
-    const newUser = new UserForm({ name, email, phone, date, project });
+    const newUser = new UserForm({ name, email, phone, date, directive, project });
     await newUser.save();
 
     // Відправляємо повідомлення в Telegram через axios
     const telegramMessage = `
+    Directive: ${directive}
       Name: ${name}
-      Phone: ${phone}
+      Phone: +1${phone}
       Email: ${email}
       Date: ${date}
       Project: ${project}
     `;
-    await axios.post(
-      `https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`,
-      {
-        chat_id: process.env.CHAT_IDS,
-        text: telegramMessage,
-      },
-    );
+    await axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage`, {
+      chat_id: process.env.CHAT_IDS,
+      text: telegramMessage,
+    });
 
     res.status(201).json({
-      message:
-        "Користувача створено успішно, повідомлення надіслано в Telegram і email",
+      message: "Користувача створено успішно, повідомлення надіслано в Telegram і email",
     });
   } catch (error) {
-    console.error(
-      "Помилка при створенні користувача або відправленні повідомлення:",
-      error,
-    );
+    console.error("Помилка при створенні користувача або відправленні повідомлення:", error);
   }
 });
 
